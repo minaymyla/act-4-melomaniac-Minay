3dcba36cdef660db870d77ae9553b01d731f5db0
minaymyla <101568144+minaymyla@users.noreply.github.com>
2022-04-19 18:31:17 +0800

    Add files via upload

---
+++ Song3.php
@@ -0,0 +1,179 @@
+<!DOCTYPE html>
+<html>
+<body style="background-color:skyblue;">
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Melomaniac Activity</title>
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+      
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="mainNav">
+            <div class="container px-4">
+                <a class="navbar-brand" href="#page-top"> Ultimate Guitar </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item"><a class="nav-link text-light" href="Song1.php">Song 1</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="Song2.php">Song 2</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="Song3.php">Song 3</a></li>
+                         <li class="nav-item"><a class="nav-link text-light" href="Song4.php">Song 4</a></li>
+                          <li class="nav-item"><a class="nav-link text-light" href="Song5.php">Song 5</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <!-- Header-->
+        <header class="bg-dark bg-gradient text-white">
+            <div class="container px-4 text-center">
+                <h1 class="fw-bolder">Welcome Hummies!</h1>
+                <p class="lead">Sometimes Its okay not to be okay, we may lost the battle but it doesn't mean that we need to give up. We just need to take a rest and after that our lives must go on.</p>
+                
+            </div>
+        </header>
+        <!-- About section-->
+        <section id="songs">
+            <div class="container px-4 ">
+                <div class="row gx-4 justify-content-center">
+                    <div class="col-lg-12 text-center">
+                        <div class = "container">
+                        <img src="images/image3.jpg" alt="..." width="190" class="rounded mb-2 img-thumbnail">
+                        
+                        <?php
+
+                        echo "<h2 class='text-primary'>It's Okay</h2>";
+                        echo "<p class='lead '>NightBird</p>";
+                
+                        echo "<p> [Intro]</p>";
+                        echo  "<h5><pre> D  Em  G G  x2</h5>";
+                        echo "<br/>";
+
+                        echo "<p> Verse 1 </p>";
+                        echo "<h5><pre>  D     Em                        G            G</h5>";
+                        echo "<p> I moved to California in the summer time</p>";
+                        echo "<h5><pre>  D  Em                            G                        G</h5>";
+                        echo "<p> I changed my name thinking that it would change my mind</p>";
+                        echo "<h5><pre>  D  Em                            G                        G</h5>"; 
+                        echo "<p> I thought that all my problems they would stay behind</p>";
+                        echo "<h5><pre>           D                        Em                   G              G</h5>";  
+                        echo "<p> I was a stick of dynamite and it was just a matter of time, yeah</p>";              
+                        echo "<br/>";
+
+                        echo "<p> Pre-Chorus 1</p>";
+                        echo "<h5><pre>  Bm           Bm</h5>";
+                        echo "<p> Oh, dang, oh, my now I can't hide</p>";
+                        echo "<h5><pre>         A                 A</h5>";
+                        echo "<p> Said I knew myself, but I guess I lied</p>";
+                        echo "<br/>";
+
+                        echo "<p>[Chorus 1]</p>";
+                        echo "<h5><pre>      D                       G</h5>";
+                        echo "<p> It's okay, it's okay, it's okay, it's okay</p>";
+                        echo "<h5><pre>            Bm                                A</h5>";
+                        echo "<p> If you're lost. We're all a little lost and it's all right</p>";                      
+                        echo "<h5><pre>       D                       G</h5>";
+                        echo "<p> It's okay, it's okay, it's okay, it's okay</p>";
+                        echo "<h5><pre>            Bm                                A</h5>";
+                        echo "<p> If you're lost. We're all a little lost and it's all right</p>";
+                        echo "<h5><pre>           D               G               Bm             A</h5>";
+                        echo "<p> It's all right, it's all right, it's all right</p>";
+                        echo "<br/>";
+
+                         echo "<p> [Verse 2]</p>";
+                        echo "<h5><pre> D       Em                        G                         G</h5>";
+                        echo "<p>   I-I wrote a hundred pages but I burned 'em all (Yeah, I burned 'em all)</p>";
+                        echo "<h5><pre>  D       Em                                 G                   G</h5>";
+                        echo "<p> I blow through yellow lights and don't look back at all (I don't look back at all)</p>";
+                        echo "<h5><pre>  D       Em                                 G               G</h5>";
+                        echo "<p>   Yeah, you can call me reckless, I'm a cannonball (I'm a cannonball)</p>";
+                        echo "<h5><pre>  D       Em                                 G               G</h5>";
+                        echo "<p>   Don't know why I take a tightrope and cry when I fall</p>";
+                        echo "<br/>";
+
+
+
+                        echo "<p>[Pre-chorus 2]</p>";
+                        echo "<h5><pre>      Bm           Bm</h5>";
+                        echo "<p> Oh, dang, oh, my now I can't hide</p>";
+                        echo "<h5><pre>        A           A</h5>";
+                        echo "<p> Said I knew what I wanted, but I guess I lied</h5>";                   
+                        echo "<br/>";
+
+                        echo "<p>[Chorus 2]</p>";
+                        echo "<h5><pre>     D                       G</h5>";
+                        echo "<p> It's okay, it's okay, it's okay, it's okay</p>";
+                        echo "<h5><pre>        Bm                                A</h5>";                      
+                        echo "<p> If you're lost. We're all a little lost and it's all right</p>";
+                        echo "<h5><pre>       D                       G</h5>";
+                        echo "<p> It's okay, it's okay, it's okay, it's okay</p>";
+                        echo "<h5><pre>            Bm                                A</h5>";
+                        echo "<p> If you're lost. We're all a little lost and it's all right</p>";                      
+                        echo "<h5><pre>           D               G               Bm             A</h5>";
+                        echo "<p> It's all right, it's all right, it's all right</p>";
+                        echo "<br/>";
+                        
+                        echo "<p> [Instrumental]</p>";
+                        echo "<h5><pre>  D G Bm A x2</h5>";
+                        echo "<br/>";
+
+                        echo "<p> [Bridge]</p>";
+                        echo "<h5><pre>  Bm     F#m    G               G</h5>";
+                        echo "<p> Oh-oh-oh-oh, it's all right</p>";
+                        echo "<h5><pre>  Bm     F#m    G               G</h5>";
+                        echo "<p> Oh-oh-oh-oh, it's all right</p>";
+                        echo "<h5><pre>  A               A</h5>";
+                        echo "<p> It's all right, it's all right</p>";
+                        echo "<h5><pre>  Bm     F#m    G              G</h5>";
+                        echo "<p> Oh-oh-oh-oh, it's all right</p>";                      
+                        echo "<h5><pre> Bm     F#m    G         A</h5>";
+                        echo "<p> Oh-oh-oh-oh, it's all right</p>";
+                        echo "<h5><pre>  Bm     F#m    G              G</h5>";
+                        echo "<p> Oh-oh-oh-oh, it's all right</p>";
+                        echo "<h5><pre>  Bm     F#m    G              G</h5>";
+                        echo "<p> Oh-oh-oh-oh, it's all right</p>";
+                        echo "<h5><pre>  Bm     F#m          G                  A</h5>";
+                        echo "<p> Oh-oh-oh-oh, it's all right     to be lost sometimes</p>";
+                        echo "<br/>";
+
+                        echo "<p>[Chorus 3]</p>";
+                        echo "<h5><pre>       D                       G</p>";
+                        echo "<p> It's okay, it's okay, it's okay, it's okay</p>";
+                        echo "<h5><pre>            Bm                                A</h5>";
+                        echo "<p> If you're lost. We're all a little lost and it's all right</p>";
+                        echo "<h5><pre>       D                       G</h5>";
+                        echo "<p> It's okay, it's okay, it's okay, it's okay</p>";
+                        echo "<h5><pre>           Bm                                A</h5>";
+                        echo "<p> If you're lost. We're all a little lost and it's all right</p>";
+                        echo "<h5><pre>           D               G               Bm             A</h5>";
+                        echo "<p> It's all right, it's all right, it's all right, it's all right yeah</p>";         
+                        echo "<br/>";
+
+                        echo "<p>OUTRO</p>";
+                        echo "<h5><pre>   D                           G</p>";
+                        echo "<p> It's okay, it's okay, it's okay, it's okay</p>";
+                        echo "<h5><pre>            Bm                                A</h5>";
+                        echo "<p> If you're lost. We're all a little lost and it's all right</p>";
+                        echo "<br/>";
+                        ?>
+
+
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <footer class="py-3 bg-danger">
+            <div class="container px-4"><p class="m-0 text-center text-white">Copyright &copy; 2022. Myla G. Minay</p></div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
