1f83c64c83c889406b9daf9f1e5c71e35968cbfb
minaymyla <101568144+minaymyla@users.noreply.github.com>
2022-04-19 16:52:28 +0800

    Add files via upload

---
+++ melomaniac-activity/example_only_for_reference.php
@@ -0,0 +1,127 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Melomaniac Activity</title>
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+      
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="mainNav">
+            <div class="container px-4">
+                <a class="navbar-brand" href="#page-top"> Tune Avenue </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item"><a class="nav-link text-light" href="song1.php">Song 1</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song2.php">Song 2</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song3.php">Song 3</a></li>
+                         <li class="nav-item"><a class="nav-link text-light" href="song4.php">Song 4</a></li>
+                          <li class="nav-item"><a class="nav-link text-light" href="song5.php">Song 5</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <!-- Header-->
+        <header class="bg-dark bg-gradient text-white">
+            <div class="container px-4 text-center">
+                <h1 class="fw-bolder">Welcome to Tune Avenue</h1>
+                <p class="lead">Melancholy runs through music like a message through a stick of rock </p>
+                
+            </div>
+        </header>
+        <!-- About section-->
+        <section id="songs">
+            <div class="container px-4">
+                <div class="row gx-4 justify-content-center">
+                    <div class="col-lg-12 text-center">
+                        
+                        <?php
+
+                        echo "<h2 class='text-primary'>Kathang Isip</h2>";
+                        echo "<p class='lead '>Ben & Ben</p>";
+                
+                        echo "<p> Diba nga ito ang iyong gusto? </p>";
+                        echo "<p> O,Ito'y lilisan na ako</p>";
+                        echo "<p> Mga alaala'y ibabaon</p>";
+                        echo "<p> Kalakip ang tamis ng kahapon</p>";
+                        echo "<br/>";
+
+                        echo "<p> Mga gabing di namamalayang</p>";
+                        echo "<p> Oras ay lumilipad</p>";
+                        echo "<p> Mga sandaling lumalayag kung</p>";
+                        echo "<p> San man tayo mapadpad</p>";
+                        echo "<p> Bawat Kilig na nadarama</p>";
+                        echo "<p> Sa tuwing hawak ang 'yong kamay</p>";
+                        echo "<p> Ito'y maling akala</p>";
+                        echo "<p> Isang malaking sablay</p>";
+                        echo "<br/>";
+
+                        echo "<p> Pasensya ka na</p>";
+                        echo "<p> Sa mga kathang isip kong ito</p>";
+                        echo "<p> Wari'y dala lang ng pagmamahal sa iyo</p>";
+                        echo "<p> Ako'y gigising na</p>";
+                        echo "<p> Sa panaginip kong ito</p>";
+                        echo "<p> At sa wakas ay kusang lalayo sa iyo (lalayo sa)</p>";
+                        echo "<br/>";
+
+                        echo "<p> Kung gaano kabilis nagsimula</p>";
+                        echo "<p> Gano'n katulin nawala</p>";
+                        echo "<p> Maaari ba tayong bumalik sa umpisa</p>";
+                        echo "<p> Upang di na umasa ang pusong nagiisa</p>";
+                        echo "<br/>";
+
+                        echo "<p> Pasensiya ka na</p>";
+                        echo "<p> Sa mga kathang isip kong ito</p>";
+                        echo "<p> Wari'y dala lang ng pagmamahal sa iyo</p>";
+                        echo "<p> Ako'y gigising na</p>";
+                        echo "<p> Sa panaginip kong ito</p>";
+                        echo "<p> At sa wakas ay kusang lalayo sa iyo (lalayo sa)</p>";
+                        echo "<br/>";
+
+                        echo "<p> Sumabay sa agos na isinulat ng tadhana</p>";
+                        echo "<p> Minsan siya'y para sa iyo</p>";
+                        echo "<p> Pero minsan siya'y paasa</p>";
+                        echo "<p> Tatakbo papalayo't</p>";
+                        echo "<p> Kakalimutan ang lahat</p>";
+                        echo "<br/>";
+
+                        echo "<p> Pero kahit saan man lumingon</p>";
+                        echo "<p> Nasusulyapan ang kahapon</p>";
+                        echo "<p> At sa aking bawat paghinga</p>";
+                        echo "<p> Ikaw ang nasa isip ko, sinta</p>";
+                        echo "<br/>";
+
+                        echo "<p> Kaya pasensya ka na</p>";
+                        echo "<p> Sa mga kathang isip kong ito</p>";
+                        echo "<p> Wari'y dala lang ng pagmamahal sa iyo</p>";
+                        echo "<p> Ako'y gigising na</p>";
+                        echo "<p> Mula sa panaginip kong ito</p>";
+                        echo "<p> At sa wakas ay kusang lalayo sa iyo (lalayo sa)</p>";
+                        echo "<br/>";
+
+                        echo "<p> Diba nga ito ang iyong gusto?</p>";
+                        echo "<p> O,Ito'y lilisan na ako</p>";
+
+                        ?>
+
+
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <footer class="py-4 bg-danger">
+            <div class="container px-4"><p class="m-0 text-center text-white">Copyright &copy; 2022. Ryan Clifford Perez</p></div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
---
+++ melomaniac-activity/index.php
@@ -0,0 +1,169 @@
+<!DOCTYPE html>
+<html>
+<body style="background-color:pink;">
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Melomaniac Activity</title>
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+      
+        <link href="css/styles.css" rel="stylesheet" />
+        <html>
+        
+    </head>
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="mainNav">
+            <div class="container px-4">
+                <a class="navbar-brand" href="#page-top"> Ultimate Guitar </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item"><a class="nav-link text-light" href="Song1.php">Song 1</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="Song2.php">Song 2</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="Song3.php">Song 3</a></li>
+                         <li class="nav-item"><a class="nav-link text-light" href="Song4.php">Song 4</a></li>
+                          <li class="nav-item"><a class="nav-link text-light" href="Song5.php">Song 5</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <!-- Header-->
+        <header class="bg-dark bg-gradient text-white">
+            <div class="container px-4 text-center">
+
+                <h1 class="fw-bolder">Welcome Hummies! </h1>
+                <p class="lead">Whenever I listen to any music it makes me calm and the peace of mind I'm looking for is in music. I love music but I don't know why music doesn't love me ;</p>
+                
+            </div>
+        </header>
+        <!-- About section-->
+        <section id="songs">
+            <div class="container px-4 ">
+                <div class="row gx-4 justify-content-center">
+                    <div class="col-lg-12 text-center">
+                        <div class = "container">
+                        <img src="images/image1.jpg" alt="..." width="180" class="rounded mb-2 img-thumbnail">
+                        
+                        <?php
+
+                        echo "<h2 class='text-primary'>Just Once</h2>";
+                        echo "<p class='lead '>James Ingram</p>";
+                
+                        echo "<p> [Intro]</p>";
+                        echo  "<h5><pre> C FM7 Em7 FM7 </h5>";
+                        echo "<br/>";
+
+                        echo "<h5><pre> C9    FM7 </h5>";
+                        echo "<p>I did my best </p>";
+                        echo "<h5><pre> Em7                        Am7</h5>";
+                        echo "<p>But I guess my best wasn't good enough</p>";
+                        echo "<h5><pre>   KDm7               G7       Em7   Am7</h5>";
+                        echo "<p>Cause here we are, back where we were before</p>"; 
+                        echo "<h5><pre>Dm7                  C/E           FM7      G7sus4 G7</h5>";
+                        echo "<p> Wonderin' if we ought to stay, or head on out the  door.</p>";                
+                        echo "<br/>";
+
+                        echo "<p> CHORUS 1</p>";
+                        echo "<h5><pre>  C     G/C              Gm7              C7              FM7>/h5>";
+                        echo "<p>Just once can we figure out what we keep doin' wrong?</p>";
+                        echo "<h5><pre> Dm7            G7           Em7 Am</h5>";
+                        echo "<p>Why we never last for very long;</p>";
+                        echo "<h5><pre> Dm7         G7sus4</h5>";
+                        echo "<p>What are we doing wrong?</p>";
+                        echo "<h5><pre> C     G/C           Gm7             C7              FM7</p>";
+                        echo  "<p>Just once, can we find a way to finally make it right,</p>";
+                        echo "<h5><pre>   Dm7              G7                Em7       Am7</h5>";
+                        echo "<p>To make the magic last for more than just one night?p>";
+                        echo "<h5><pre>    Dm7                        G7sus4                  C9 FM7  C FM7 G</h5>";
+                        echo  "<p>If we could just get to it, I know we could break through it.     hmmm.</p>";
+                        echo "<br/>";
+
+                        echo "<p> VERSE 2</p>";
+                        echo "<h5><pre>C9       FM7</h5>";
+                        echo"<p>I gave my all, </p>";
+                        echo "<h5><pre> Em7               Am7<h5>";
+                        echo "<p>But think all my have been too much:</p>";
+                        echo "<h5><pre>     Dm7                  G7         Em7 Am</h5>";
+                        echo"<p>The lord knows we're not getting anywhere.</h5>";
+                        echo "<h5><pre> Dm7          G7sus4 G7</p>";
+                        echo"<p>    Seems we're always blowin'</p>";
+                        echo "<h5><pre>  E7sus4  E7          Am7";
+                        echo "<p>What ever we've got goin',</p>";
+                        echo "<h5><pre>        Dm7                 C                 FM7      G7sus4 G7";
+                        echo "<p>And it seems that time was all we've got; We haven't got a prayer.</p>";
+                        echo "<br/>";
+
+                        echo "<p> CHORUS 2</p>";
+                        echo "<h5><pre>C      G/C             Gm7            C7             FM7</h5>";
+                        echo "<p>Just once, can we figure out what we keep doin' wrong,</p>";
+                        echo "<h5><pre> Dm7                  G7           Em7  Am</p>";
+                        echo "<p>Why the good times never last for long?</p>";
+                        echo "<h5><pre>           Dm7 G7sus4 G7</h5>";
+                        echo "<p> Where are we goin wrong? </p>";
+                        echo "<h5><pre> C    G/C             Gm7             C7            FM7</h5>";
+                        echo "<p> Just once, can we find a way to finally make it right,</p>";
+                        echo "<h5><pre>Dm7              G7                Em7         Am7</h5>";
+                        echo "<p>To make the magic last for more than just one night?</p>";
+                        echo "<h5><pre> Dm7</h5>";
+                        echo "<p>    I know we could break through it,</p>";
+                        echo "<h5><pre>     G7sus4 </h5>";
+                        echo "<p>If we could just get to it.</p>";
+                        echo "<br/>";
+
+                        echo "<p> BRIDGE</p>";
+                        echo "<h5><pre>      Ab               Fm7>/h5>";
+                        echo "<p>Just once, I want to understand</p>";
+                        echo "<h5> Bbm7          Eb7sus4        Ab</h5>";
+                        echo "<p>Why it always comes back to goodbyes.</p>";
+                        echo "<h5> B  BM7               G#m7</h5>";
+                        echo" Why can't we get ourselves in hand,</p>";
+                        echo "<h5>  C#m7           B/D#</h5>";
+                        echo "<p>And admit to one another</p>";
+                        
+                        echo "<h5>  E               B/D#</h5>";
+                        echo " <p>We're no good without each other?</p>";
+                        echo "<h5>  C#m7                B/D#          Em7           A7sus4 A7</h5>";
+                        echo "<p>Take the best and make it better; Find a way to stay together.</p>";
+                        echo "<br/>";
+
+                        echo "<p> CHORUS 3</p>";
+                        echo "<h5><pre> D         A/D    Am9          D7/F#        GM7  (F#m7)</h5>";
+                        echo "<p>  Just once, can we find a way to fin'lly make it right,  oh</p>";
+                        echo "<h5><pre> Em7                A7/G          F#m7      Bm7</h5>";
+                        echo "<p>to make the magic last for more than just one night?</p>";
+                        echo "<h5><pre> Em7</h5>";
+                        echo "<p>I know we could break through it</p>";
+                        echo "<h5><pre>    A7sus4          A7</h5>";
+                        echo "<p> If we could just get to it.</p>";
+                        echo "<h5><pre>D A/D G/D A/D</h5>";
+                        echo "<p>   ....just once.</p>";
+                        echo "<h5><pre> D  A/D    Bb    A7 (hold)</h5>";
+                        echo "<p>  Woh, we can get to it</p>";
+                        echo "<p>        (Coda)</p>";
+                        echo "<p>   just once.</p>";
+                        echo "<br/>";
+
+                        echo "<p>OUTRO</p>";
+                        echo "<h5><pre>D G D/F#  G9  A7sus4 B</h5>";
+                        echo "<br/>";
+                        ?>
+
+
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <footer class="py-3 bg-danger">
+            <div class="container px-4"><p class="m-0 text-center text-white">Copyright &copy; 2022. Myla G. Minay</p></div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
