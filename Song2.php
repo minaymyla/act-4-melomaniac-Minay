94f00c62cce6355787071f935ff07988a7dfed99
minaymyla <101568144+minaymyla@users.noreply.github.com>
2022-04-19 17:37:28 +0800

    Add files via upload

---
+++ Song2.php
@@ -0,0 +1,154 @@
+<!DOCTYPE html>
+<html>
+<body style="background-color:pink;">
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Melomaniac Activity</title>
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+      
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="mainNav">
+            <div class="container px-4">
+                <a class="navbar-brand" href="#page-top"> Ultimate Guitar </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item"><a class="nav-link text-light" href="Song1.php">Song 1</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="Song1.php">Song 2</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="Song1.php">Song 3</a></li>
+                         <li class="nav-item"><a class="nav-link text-light" href="Song1.php">Song 4</a></li>
+                          <li class="nav-item"><a class="nav-link text-light" href="Song1.php">Song 5</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <!-- Header-->
+        <header class="bg-dark bg-gradient text-white">
+            <div class="container px-4 text-center">
+                <h1 class="fw-bolder">Welcome Hummies!</h1>
+                <p class="lead">There is one perso that will always be there for you through your ups and downs and you can call him/her a friend. Ahving atleast one friend is enough as long as it is true and genuine.</p>
+                
+            </div>
+        </header>
+        <!-- About section-->
+        <section id="songs">
+            <div class="container px-4 ">
+                <div class="row gx-4 justify-content-center">
+                    <div class="col-lg-12 text-center">
+                        <div class = "container">
+                        <img src="images/image4.JPG" alt="..." width="180" class="rounded mb-2 img-thumbnail">
+                        
+                        <?php
+
+                        echo "<h2 class='text-primary'>That's What Friends are For</h2>";
+                        echo "<p class='lead '>Dionne Warwwick</p>";
+                
+                        echo "<p> [Verse]</p>";
+                        echo "<h5><pre> D         F#m       Bm       Em </h5>";
+                        echo "<p>And I never thought I'd feel this way</p>";
+                        echo "<h5>                      C#m7</h5>";
+                        echo "<p>And as far as I'm concerned</p>";
+                        echo "<h5><pre>      F#7                     Bm</h5>";
+                        echo "<p>I'm glad I got the chance to say </p>";
+                        echo "<h5><pre>     G               A</h5>"; 
+                        echo "<p>That I do believe I love you</p>";
+                        echo "<h5><pre>   D           F#m  Bm   Em</h5>";  
+                        echo "<p> And if I should ever go away</p>";  
+                        echo "<h5><pre>            Em7                 C#m7</h5>";            
+                        echo "<p> Well then close your eyes and try</p>"; 
+                        echo "<h5><pre>             F#7          Bm</h5>";            
+                        echo "<p> To feel the way we  do today </p>";            
+                        echo "<h5><pre>          G             A</h5>";
+                        echo "<p> and then if you can remember</p>";
+                        echo "<br/>";
+
+                        echo "<p> CHORUS 1</p>";
+                        echo "<h5><pre>  C     G/C              Gm7              C7              FM7>/h5>";
+                        echo "<p>Just once can we figure out what we keep doin' wrong?</p>";
+                        echo "<h5><pre> Dm7            G7           Em7 Am</h5>";
+                        echo "<p>Why we never last for very long;</p>";
+                        echo "<h5><pre> Dm7         G7sus4</p>";
+                        echo "<p>What are we doing wrong?</p>";
+                        echo "<h5><pre> C     G/C           Gm7             C7              FM7</h5>";
+                        echo  "<p>Just once, can we find a way to finally make it right,</p>";
+                        echo "<h5><pre>   Dm7              G7                Em7       Am7</h5>";
+                        echo "<p>To make the magic last for more than just one night?p>";
+                        echo "<h5><pre>    Dm7                        G7sus4                  C9 FM7  C FM7 G</h5>";
+                        echo  "<p>If we could just get to it, I know we could break through it.     hmmm.</p>";
+                        echo "<br/>";
+
+                        echo "<p> Chorus</p>";
+                        echo "<h5><pre>D             F#m</p>";
+                        echo"<p>Keep smiling, keep shining</p>";
+                        echo "<h5><pre>  G                              F#m    Bm<h5>";
+                        echo "<p>Knowing you can always count on me, for sure</p>";
+                        echo "<h5><pre>  G                     A</h5>";
+                        echo"<p>That's what friends are for</p>";
+                        echo "<h5><pre>D               F#m</h5>";
+                        echo"<p>For good times, and bad times</p>";
+                        echo "<h5><pre>D7               B7          Em</h5>";
+                        echo "<p>I'll be on your side forever more</p>";
+                        echo "<h5><pre> A</h5>";
+                        echo "<p> That's what friends are for</p>";
+                        echo "<br/>";
+
+                        echo "<p>[Instrumental] </p>"; 
+                        echo "<h5><pre>D         F#m       Bm       Em</h5>";
+                        echo "<br/>";
+
+                        echo "<p> Verse</p>";
+                        echo "<h5><pre>          Em             C#m7</h5>";
+                        echo "<p> Well you came and opened me</p>";
+                        echo "<h5><pre>                F#m             Bm</h5>";
+                        echo "<p> And now there's so much more I see</p>";
+                        echo "<h5><pre>        G                A</h5>";
+                        echo "<p> And so by the way I thank you </p>";
+                        echo "<h5><pre>     D            F#m         Bm     Em</h5>";
+                        echo "<p> And then for the times when we're apart</p>";
+                        echo "<h5><pre>                                C#m7</h5>";
+                        echo "<p> Well then close your eyes and know</p>";
+                        echo "<h5><pre>              F#7        Bm</h5>";
+                        echo "<p> These words are coming from my heart</p>";
+                        echo "<h5><pre> G                        A</h5>";
+                        echo "<p> And then if you can remember</p>";
+                        echo "<br/>";
+
+                        echo "<p> Chorus</p>";
+                        echo "<h5><pre> D                F#m</h5>";
+                        echo "<p> Keep smiling, keep shining</p>";
+                        echo "<h5><pre>  G              Em              F#m    Bm</h5>";
+                        echo "<p> Knowing you can always count on me for sure</p>";
+                        echo "<h5><pre>   G                      A</h5>";
+                        echo "<p> That's what friends are for</p>";
+                        echo "<h5><pre> D                  F#m</h5>";
+                        echo "<p> For good times, and bad times</p>";
+                        echo "<h5><pre> D7               B7          Bb6   Bbmaj7 Bb6</h5>";
+                        echo "<p> I'll be on your side forever more</p>";
+                        echo "<h5><pre> D                       A7sus   A7</h5>";
+                        echo "<p> That's what friends are for</p>";
+                        echo "<br/>";
+
+                        ?>
+
+
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <footer class="py-3 bg-danger">
+            <div class="container px-4"><p class="m-0 text-center text-white">Copyright &copy; 2022. Myla G. Minay</p></div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
