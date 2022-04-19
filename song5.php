0d380539d298a460c8a226308b9b820d3ae1609f
minaymyla <101568144+minaymyla@users.noreply.github.com>
2022-04-19 18:41:01 +0800

    Add files via upload

---
+++ song5.php
@@ -0,0 +1,137 @@
+<!DOCTYPE html>
+<html>
+<body style="background-color:pink;">
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Melomaniac Activity</title>
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+      
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="mainNav">
+            <div class="container px-4">
+                <a class="navbar-brand" href="#page-top"> Ultimate Guitar </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item"><a class="nav-link text-light" href="Song1.php">Song 1</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="Song2.php">Song 2</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="Song3.php">Song 3</a></li>
+                         <li class="nav-item"><a class="nav-link text-light" href="Song4.php">Song 4</a></li>
+                          <li class="nav-item"><a class="nav-link text-light" href="Song5.php">Song 5</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <!-- Header-->
+        <header class="bg-dark bg-gradient text-white">
+            <div class="container px-4 text-center">
+                <h1 class="fw-bolder">Welcome Hummies!</h1>
+                <p class="lead">The best things in this world are indeed great blessings to us and we can really say tha despite of all the bad things that happens around us we still have a Wonderful World. </p>
+                
+            </div>
+        </header>
+        <!-- About section-->
+        <section id="songs">
+            <div class="container px-4 ">
+                <div class="row gx-4 justify-content-center">
+                    <div class="col-lg-12 text-center">
+                        <div class = "container">
+                        <img src="images/image6.jpg" alt="..." width="200" class="rounded mb-2 img-thumbnail">
+                        
+                        <?php
+
+                        echo "<h2 class='text-primary'>Grow Old with You</h2>";
+                        echo "<p class='lead '>Adam Sandler</p>";
+                
+                        
+
+                        echo "<p> Verse 1 </p>";
+                        echo "<h5><pre>            A    </h5>";
+                        echo "<p> I wanna make you smile</p>";
+                        echo "<h5><pre>Bm</h5>";
+                        echo "<p> Whenever you're sad</p>";
+                        echo "<h5><pre> C#m </h5>"; 
+                        echo "<p> Carry you around</p>";
+                        echo "<h5><pre>               D</h5>";
+                        echo "<p> When your arthritis is bad</p>";
+                        echo "<h5><pre> A                  E</h5>";
+                        echo "<p> All I wanna do is</p>";
+                        echo "<h5><pre>  D         A     E </h5>"; 
+                        echo "<p> Grow old with you.</p>";               
+                        echo "<br/>";
+
+                        echo "<p> [Verse 2]</p>";
+                        echo "<h5><pre>               A  </h5>";
+                        echo "<p> I'll get you medicine</p>";
+                        echo "<h5><pre> Bm</h5>";
+                        echo "<p> When your tummy aches</p>";
+                        echo "<h5><pre> C#m</h5>";
+                        echo "<p> Build you a fire</p>";
+                        echo "<h5><pre>            D</h5>";
+                        echo "<p> When the furnace breaks</p>";
+                        echo "<h5><pre> A                    E</h5>";
+                        echo "<p> It could be so nice</p>";
+                        echo "<h5><pre>         D         A    A7</h5>";
+                        echo "<p> Growing old with you.</p>";
+                        echo "<br/>";
+                        
+
+                        echo "<p>[Chorus]</p>";
+                        echo "<h5><pre>       D</h5>";
+                        echo "<p> I'll miss you</p>";
+                        echo "<h5><pre>  D</h5>";
+                        echo "<p> I'll kiss you</p>";
+                        echo "<h5><pre>A</h5>";
+                        echo "<p> Give you my coat when you are cold</p>";
+                        echo "<h5><pre> D</h5>";
+                        echo "<p> Need you</p>";
+                        echo "<h5><pre> E(hold)           E      D</h5>";
+                        echo "<p> Even let you hold the remote control</p>";
+                        echo "<br/>";
+
+
+                        echo "<p>[Verse 3]</p>";
+                        echo "<h5><pre> A</h5>";
+                        echo "<p> Let me do the dishes</p>";
+                        echo "<h5><pre>Bm</h5>";                      
+                        echo "<p> In our kitchen sink</p>";                      
+                        echo "<h5><pre> C#m</h5>";
+                        echo "<p> Put you to bed</p>";
+                        echo "<h5><pre> </h5>";
+                        echo "<p> When you've had too much to drink</p>";
+                        echo "<h5><pre> A                       E</h5>";
+                        echo "<p> I could be the man who</p>";
+                        echo "<h5><pre>       D        A</h5>";                      
+                        echo "<p> Grows old with you</p>";                      
+                        echo "<h5><pre>    E          D        A</h5>";
+                        echo "<p> I wanna grow old with you</p>";
+                        echo "<br/>";
+
+                       
+                        
+
+
+                        ?>
+
+
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <footer class="py-3 bg-danger">
+            <div class="container px-4"><p class="m-0 text-center text-white">Copyright &copy; 2022. Myla G. Minay</p></div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
