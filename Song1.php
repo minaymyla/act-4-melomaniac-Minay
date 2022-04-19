d1c31e8f45c54d299f90a9291f8ebe2b105ccc8e
minaymyla <101568144+minaymyla@users.noreply.github.com>
2022-04-19 17:18:48 +0800

    Add files via upload

---
+++ Song1.php
@@ -0,0 +1,117 @@
+<!DOCTYPE html>
+<html>
+<body style="background-color:pink;">
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Melomaniac Activity</title>
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+      
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="mainNav">
+            <div class="container px-4">
+                <a class="navbar-brand" href="#page-top"> Ultimate Guitar </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item"><a class="nav-link text-light" href="Song1.php">Song 1</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="Song2.php">Song 2</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="Song3.php">Song 3</a></li>
+                         <li class="nav-item"><a class="nav-link text-light" href="Song4.php">Song 4</a></li>
+                          <li class="nav-item"><a class="nav-link text-light" href="Song5.php">Song 5</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <!-- Header-->
+        <header class="bg-dark bg-gradient text-white">
+            <div class="container px-4 text-center">
+                <h1 class="fw-bolder">Welcome Hummies!</h1>
+                <p class="lead">Sometimes you need peace of mind and you can find it while looking the moom while listening to music</p>
+                
+            </div>
+        </header>
+        <!-- About section-->
+        <section id="songs">
+            <div class="container px-4 ">
+                <div class="row gx-4 justify-content-center">
+                    <div class="col-lg-12 text-center">
+                        div class = "container">
+                        <img src="images/image2.jpg" alt="..." width="180" class="rounded mb-2 img-thumbnail">
+                        
+                        <?php
+
+                        echo "<h2 class='text-primary'>Moonlight Over Paris</h2>";
+                        echo "<p class='lead '>Paolo Santos</p>";
+                
+                        echo "<p> [VERSE 1]</p>";
+                        echo "<h5><pre> D                               Em7 </h5>";
+                        echo "<p>You say you've been overseas</p>";
+                        echo "<h5><pre>C              D</h5>";
+                        echo "<p>I say over where</p>";
+                        echo "<h5><pre> D                      Em7</h5>";
+                        echo "<p>You say just a holiday</p>"; 
+                        echo "<h5><pre>C                 D</h5>";
+                        echo "<p>My Alsatian heir </p>";
+                        echo "<h5><pre> F                      C                  F                   C</h5>";
+                        echo "<p> I say I've been working late, working overtime</p>";
+                        echo "<h5><pre> D                         Em7                    C   C/D</h5>";
+                        echo "<p> Haven't seen the sun since eighty-nine</p>";                
+                        echo "<br/>";
+
+                        echo "<p> CHORUS </p>";
+                        echo "<h5><pre>G                             C/E </h5>";
+                        echo "<p>Does the moonlight shine on Paris</p>";
+                        echo "<h5><pre> C                            G</h5>";
+                        echo "<p>After the sun goes down</p>";
+                        echo "<h5><pre> G                   C/E</h5>";
+                        echo "<p>If the London Bridge is falling</p>";
+                        echo "<h5><pre>         C                      D</h5>";
+                        echo  "<p>Will anybody hear a sound</p>";
+                        echo "<h5><pre>Am7                  G/B       C             G/B</h5>";
+                        echo "<p>If you follow the sunset will it ever end</p>";
+                        echo "<h5><pre>Am7        G/B           C</h5>";
+                        echo "<p>Does the moonlight shine on Paris</p>";
+                        echo "<h5><pre>Am7      D        Intro (2x only)</h5>";
+                        echo "<p>Will it ever end</p>";
+                        echo "<br/>";
+
+                        echo "<p> VERSE 2</p>";
+                        echo "<h5><pre>D                                      Em7</h5>";
+                        echo"<p> Oh and how can you just walk away</p>";
+                        echo "<h5><pre>        C                       D <h5>";
+                        echo "<p>Is it something that I said</p>";
+                        echo "<h5><pre>              Em7</h5>";
+                        echo"<p>You see green and red</p>";
+                        echo "<h5><pre> F                          C </h5>";
+                        echo"<p> You believe in the miracles</p>";
+                        echo "<h5><pre> F               C</h5>";
+                        echo "<p> Water into wine</p>";
+                        echo "<h5><pre> D                           Em7                           C       C/D</h5>";
+                        echo "<p> I believe it when it makes the New York Times </p>";
+                        echo "<br/>";
+
+                       
+                        ?>
+
+
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <footer class="py-3 bg-danger">
+            <div class="container px-4"><p class="m-0 text-center text-white">Copyright &copy; 2022. Myla G. Minay</p></div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
